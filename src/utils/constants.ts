7324f7a7e6f1a2e091397c3a182b273aede408b9
Champs <bruno.hrx@gmail.com>
2025-10-02 14:12:11 -0400

    change

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-// export const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? 'https://apiepantanal.com.br';
-export const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5132';
+export const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? 'https://apiepantanal.com.br';
+// export const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5132';
 
 export const headers = () => ({
   "Content-Type": "application/json",
